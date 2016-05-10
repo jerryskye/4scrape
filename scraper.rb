7084db7930bf106f30665f822b7e0d38a7328ab1
jerryskye <jarek@siedlarz.com>
2016-05-10 17:55:44 +0200

    working scraper

--- scraper.rb
+++ scraper.rb
@@ -1,27 +1,36 @@
 require 'pry'
+require 'nokogiri'
+require 'open-uri'
 require 'httparty'
 require 'cgi'
 
-def show_threads(board)
-	kek = HTTParty.get("http://boards.4chan.org/#{board}/catalog")
-	kek = kek[/catalog = .*/]
-	kek = kek[0, kek.index(/var \w+ =/)].rstrip.chop
-	kek = eval kek
+def show_threads(board, path = "dw")
+	kek = HTTParty.get("http://boards.4chan.org/#{board}/catalog")[/catalog = .*/]
+	kek = eval(kek[0, kek.index(/var \w+ =/)].rstrip.chop)
 	kek[:threads].each do |thread|
 		puts CGI.unescapeHTML "\nThread ##{thread[0].to_s}(R:#{thread[1][:r]}/I:#{thread[1][:i]}): #{thread[1][:sub]}"
 		puts CGI.unescapeHTML thread[1][:teaser].slice(0, 100)
 		print "Scrape? (y/n/q) "
 		case gets.chomp.downcase
 		when "y", "yes"
-			scrape_thread(board, thread[0].to_s)
+			scrape_thread(board, thread[0].to_s, path)
 		when "q", "quit", "exit"
 			break
 		end
 	end
 end
 
-def scrape_thread(board, thread)
-	uri = "http://boards.4chan.org/#{board}/#{thread}"
+def scrape_thread(board, thread, path)
+	uri = "http://boards.4chan.org/#{board}/thread/#{thread}"
 	puts "Scraping #{uri}"
+	dir = "#{path}/#{thread}"
+	Dir.mkdir(dir) unless Dir.exists?(dir)
+	kek = Nokogiri::HTML(open(uri)).css('form#delform > div.board > div.thread > div.postContainer')
+	kek.each do |post|
+		link = post.css('div.post > div.file > a.fileThumb').first
+		next if link.nil?
+		link = link["href"].prepend("http:")
+		File.write("dw/#{thread}/#{link[/\d+\.\w+$/]}", HTTParty.get(link))
+	end
 end
 pry
