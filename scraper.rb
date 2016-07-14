82b0feb0b697f6b83b133745e909721702064063
jerryskye <jarek@siedlarz.com>
2016-07-14 22:31:44 +0800

    Added a progressbar

--- scraper.rb
+++ scraper.rb
@@ -1,7 +1,7 @@
 require 'nokogiri'
-require 'open-uri'
 require 'httparty'
 require 'cgi'
+require 'ruby-progressbar'
 
 def show_threads(board, path)
 	kek = HTTParty.get("http://boards.4chan.org/#{board}/catalog")[/catalog = .*/]
@@ -22,27 +22,23 @@ end
 
 def scrape_thread(board, thread, path)
 	uri = "http://boards.4chan.org/#{board}/thread/#{thread[0]}"
-	print "Scraping thread ##{thread[0]}:   0.0%" 
-	dir = "#{path}/#{thread[0]}"
+	dir = "#{path}/#{thread[0]}_#{thread[1][:sub]}"
 	Dir.mkdir(dir) unless Dir.exists?(dir)
-	kek = Nokogiri::HTML(open(uri)).css('form#delform > div.board > div.thread > div.postContainer')
-	i = 0
-	max = (thread[1][:i] + 1).to_f
+	kek = Nokogiri::HTML(HTTParty.get(uri)).css('form#delform > div.board > div.thread > div.postContainer')
+	max = thread[1][:i] + 1
+	progress = ProgressBar.create(:title => "Scraping thread ##{thread[0]}", :total => max, :format => "%t: |%w|")
 	kek.each do |post|
 		link = post.css('div.post > div.file > a.fileThumb').first
 		next if link.nil?
 		link = link["href"].prepend("http:")
 		File.write("#{dir}/#{link[/\d+\.\w+$/]}", HTTParty.get(link))
-		i += 1
-		printf "\b\b\b\b\b\b%5.1f%%", (i / max * 100)
+		progress.increment
 	end
-	puts
 end
 
-path = ARGV.include?("-p") ? ARGV[ARGV.index("-p") + 1] : "dw"
-Dir.mkdir(path) unless Dir.exists?(path)
-if board = ARGV.index("-b")
-	show_threads(ARGV[board + 1], path)
+if ARGV.count == 2
+	Dir.mkdir(ARGV[1]) unless Dir.exists?(ARGV[1])
+	show_threads(ARGV[0], ARGV[1])
 else
-	puts "Usage: ruby scraper.rb -b board [-p path]"
+	puts "Usage: ruby scraper.rb board path"
 end
